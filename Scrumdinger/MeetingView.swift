8c41612c38013984ac3d7ddb1d238b3ac29b396a
Naoyuki Sato <satou30@gmail.com>
2021-06-05 12:32:07 +0900

    Managing State and Life Cycle Create an Overlay View

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -8,33 +8,39 @@
 import SwiftUI
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
     var body: some View {
-        VStack {
-            ProgressView(value: 5.0, total: 15)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.color)
+            VStack {
+                ProgressView(value: 5.0, total: 15)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Seconds Elapsed")
+                            .font(.caption)
+                        Label("300", systemImage: "hourglass.bottomhalf.fill")
+                    }
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text("Seconds Remaining")
+                            .font(.caption)
+                        Label("600", systemImage: "hourglass.tophalf.fill")
+                    }
                 }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Seconds Remaining")
-                        .font(.caption)
-                    Label("600", systemImage: "hourglass.tophalf.fill")
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel(Text("Time remaining"))
+                Circle()
+                                .strokeBorder(lineWidth: 24, antialiased: true)
+                HStack{
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}) {
+                                        Image(systemName: "forward.fill")
+                                    }
                 }
             }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel(Text("Time remaining"))
-            Circle()
-                            .strokeBorder(lineWidth: 24, antialiased: true)
-            HStack{
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}) {
-                                    Image(systemName: "forward.fill")
-                                }
-            }
+            
         }
         .padding()
     }
@@ -42,7 +48,6 @@ struct MeetingView: View {
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
-            .previewDevice("iPhone 12 Pro Max")
+        MeetingView(scrum: .constant(DailyScrum.data[0]))
     }
 }
